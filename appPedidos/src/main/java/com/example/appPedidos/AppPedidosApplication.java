863d4e8a7455b532234ca931846e023371ec6e0c
samuelito1218 <samuel.arredondo@uao.edu.co>
2025-03-11 16:34:13 -0500

    Agrego la estructura de la Api del proyecyo

---
+++ appPedidos/src/main/java/com/example/appPedidos/AppPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.appPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPedidosApplication.class, args);
+	}
+
+}
---
+++ appPedidos/src/test/java/com/example/appPedidos/AppPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
